1b8f9b6e3cd33dd0be69588af3d40803f3e23d91
Chase Starr <chasestarr@gmail.com>
2018-02-10 16:47:02 -0800

    tests

--- index.test.js
+++ index.test.js
@@ -1,18 +1,38 @@
-var postcss = require('postcss');
+const postcss = require('postcss');
 
-var plugin = require('./');
+const plugin = require('./');
 
 function run(input, output, opts) {
-    return postcss([plugin(opts)])
-        .process(input)
-        .then(result => {
-            expect(result.css).toEqual(output);
-            expect(result.warnings().length).toBe(0);
-        });
+  return postcss([plugin(opts)])
+    .process(input, { from: undefined })
+    .then(result => {
+      expect(result.css).toEqual(output);
+      expect(result.warnings().length).toBe(0);
+    });
 }
 
-/* Write tests here
-it('does something', () => {
-    return run('a{ }', 'a{ }', { });
+describe('css comparisons', () => {
+  it('converts a whole value', () => {
+    const input = ' .rule { font-size: 1rem&; }';
+    const output = ' .rule { font-size: 0.625rem; }';
+    return run(input, output);
+  });
+
+  it('converts a decimal value', () => {
+    const input = ' .rule { font-size: 3.2rem&; }';
+    const output = ' .rule { font-size: 2rem; }';
+    return run(input, output);
+  });
+
+  it('converts value with multiple &rem instances', () => {
+    const input = ' .rule { padding: 2rem& 3.2rem& 1.6rem&; }';
+    const output = ' .rule { padding: 1.25rem 2rem 1rem; }';
+    return run(input, output);
+  });
+
+  it('should not convert standard rem instances', () => {
+    const input = ' .rule { padding: 2rem 3.2rem 1.6rem; }';
+    const output = ' .rule { padding: 2rem 3.2rem 1.6rem; }';
+    return run(input, output);
+  });
 });
-*/
