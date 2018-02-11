e4b0ab3445d7adde70877800fe29a360ae3759d3
Chase Starr <chasestarr@gmail.com>
2018-02-10 16:47:14 -0800

    plugin

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-var postcss = require('postcss');
+// pulled most of the code from https://github.com/cuth/postcss-pxtorem
 
-module.exports = postcss.plugin('PLUGIN_NAME', function(opts) {
-    opts = opts || {};
+const postcss = require('postcss');
+const remRegex = require('./rem-unit-regex');
 
-    // Work with options here
+function toFixed(number, precision) {
+  const multiplier = Math.pow(10, precision + 1),
+    wholeNumber = Math.floor(number * multiplier);
+  return Math.round(wholeNumber / 10) * 10 / multiplier;
+}
 
-    return function(root, result) {
-        // Transform CSS AST here
-    };
-});
+function remReplace(m, $1) {
+  if (!$1) return m;
+  const value = parseFloat($1);
+  const fixedVal = toFixed(value / 1.6, 5);
+  return fixedVal === 0 ? '0' : fixedVal + 'rem';
+}
+
+function plugin(root) {
+  root.walkRules(rule => {
+    rule.walkDecls(decl => {
+      // This should be the fastest test and will remove most declarations
+      if (decl.value.indexOf('rem&') === -1) return;
+
+      decl.value = decl.value.replace(remRegex, remReplace);
+    });
+  });
+}
+
+module.exports = postcss.plugin('never-cursed', () => plugin);
